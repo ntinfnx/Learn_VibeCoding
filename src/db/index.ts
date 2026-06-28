b2f750307a7ab6a1d62fd192df93548c016a6a68
DimasRE <ntinfnx@gmail.com>
2026-06-28 20:17:01 +0700

    Delete src directory

--- src/db/index.ts
+++
@@ -1,9 +0,0 @@
-import { drizzle } from 'drizzle-orm/postgres-js';
-import postgres from 'postgres';
-import * as schema from './schema';
-
-const connectionString = process.env.DATABASE_URL || 'postgresql://postgres:postgres@localhost:5432/belajar_vibecoding';
-
-const client = postgres(connectionString, { prepare: false });
-
-export const db = drizzle(client, { schema });
